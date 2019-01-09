300ede50ae06e208d61a7229f56ce4cd98388449
jbosegre <jbose@mail.greenriver.edu>
2019-01-09 13:37:31 -0500

    added the composer and index page for the staging area

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jbosegre
+ * Date: 1/9/2019
+ * Time: 10:08 AM
+ */
+
+//Require the autoload file
+
+ini_set ("display_errors", 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+$f3->set('DEBUG', 3);
+
+//define a default route
+$f3->route('GET /', function (){
+    echo '<h1>Howdy Dooty!</h1>';
+
+    $view = new View();
+});
+
+//Run fat Free
+$f3->run();
